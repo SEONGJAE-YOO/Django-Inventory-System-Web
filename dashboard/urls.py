6d5ec26606f60e2ef9d5c6b66f32e643a91d0466
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-04 20:18:31 +0900

    update: urls.py( path 4개 추가)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('staff/',views.staff, name='staff')
+    path('', views.index, name='dashboard-index'),
+    path('staff/',views.staff, name='dashboard-staff'), # topnav.html에 url 연결시켜주기
+    path('product/',views.product, name='dashboard-product'),
+    path('order/',views.order, name='dashboard-order'),
 ]
