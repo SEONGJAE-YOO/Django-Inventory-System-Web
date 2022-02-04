a6d5117bb326841169501812fd3c79f8c4b6bf90
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-04 20:53:27 +0900

    update: models.py (class Product(models.Model) 생성)

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
+from calendar import c
+from sre_constants import CATEGORY
+from unicodedata import category, name
 from django.db import models
 
-# Create your models here.
+CATEGORY = (
+    ('Stationary','Stationary'),
+    ('Electronics','Electronics'),
+    ('Food','Food'),
+
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
+
