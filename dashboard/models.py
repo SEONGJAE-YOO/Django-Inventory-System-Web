6d77d480832b59c1c2ebf67d8d2de2e1ba6a0b70
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-05 15:23:22 +0900

    update: models.py (Order 클래스 , db 기능 추가)

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,11 @@
 from calendar import c
+from curses.ascii import NUL
+from datetime import date
 from sre_constants import CATEGORY
 from unicodedata import category, name
 from django.db import models
+from numpy import product
+from django.contrib.auth.models import User
 
 CATEGORY = (
     ('Stationary','Stationary'),
@@ -19,3 +23,15 @@ class Product(models.Model):
 
     def __str__(self):   # Product 제품 이름 보이게 설정
         return f'{self.name}'
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User,models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True )
+    date = models.DateTimeField(auto_now_add=True)
+
+# str : 인스턴스 출력 형식 함수
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
+
+
