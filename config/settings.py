9455800776c93b8eb2cfc27fb24a872ea154c875
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-05 17:10:19 +0900

    update: settings.py ( 'user.apps.UserConfig', 'crispy_forms' 추가 시킴)
    CRISPY_TEMPLATE_PACK = 'bootstrap4'
    bootstrap4 탬플릿 사용함

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig',
+    'user.apps.UserConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
