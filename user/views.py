8caf1d8d5e0002ffc7e2546fb787b0d5b70dbe94
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-05 17:52:01 +0900

    update: view.py ( UserCreationForm 을 CreateUserForm으로 변경)

--- user/views.py
+++ user/views.py
@@ -1,15 +1,17 @@
 from multiprocessing import context
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
 
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid(): #유효하면 저장하라
             form.save()
             return redirect('dashboard-index') 
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
     context = {
         'form':form,
 
