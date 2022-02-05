d65e8ff8a96843e98e9b00cb03e3b6565d65a67f
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-05 17:00:55 +0900

    update: models.py (Meta 내부 클래스 추가)

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,8 @@
 from calendar import c
-from curses.ascii import NUL
-from datetime import date
 from sre_constants import CATEGORY
+from tabnanny import verbose
 from unicodedata import category, name
 from django.db import models
-from numpy import product
 from django.contrib.auth.models import User
 
 CATEGORY = (
@@ -20,6 +18,10 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY, null=True)
     quantity = models.PositiveIntegerField(null=True)
 
+    # Meta 옵션이란 다음과 같이 Inner class로 사용하여 상위 클래스에게 meta data를 제공하는것입니다.
+    class Meta:
+        verbose_name_plural = 'Product'
+
 
     def __str__(self):   # Product 제품 이름 보이게 설정
         return f'{self.name}'
@@ -30,6 +32,8 @@ class Order(models.Model):
     order_quantity = models.PositiveIntegerField(null=True )
     date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = 'Order'
 # str : 인스턴스 출력 형식 함수
     def __str__(self):
         return f'{self.product} ordered by {self.staff.username}'
