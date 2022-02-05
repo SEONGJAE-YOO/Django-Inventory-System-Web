e41f6550f546cef57037d0c0be8c335f837d4417
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-05 14:15:20 +0900

    db 기능 추가(product 기능)

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,17 @@
+from csv import list_dialects
 from django.contrib import admin
 from .models import Product
+from django.contrib.auth.models import Group
+
+# 맨위 상단 이름 변경
+admin.site.site_header = '재고 관리 시스템 관리자 페이지'
 
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category','quantity')
+    list_filter = ['category']
+
+
+
+admin.site.register(Product, ProductAdmin)
+# admin.site.unregister(Group)  # Group 기능 제거할수 있는 방법
