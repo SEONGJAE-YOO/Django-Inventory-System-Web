481e6c669bf7d22350f050e1d9c17e2ab607ba35
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-07 20:12:27 +0900

    update: urls.py( user-login path 변경)

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')), #dashboard 폴더에 urls 파일 경로 설정
     path('register/', user_view.register, name = 'user-register'),
-    path('login/',auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
+    path('',auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
 
 ]
