7a997564076386b57282859c84a816e2161677dd
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-09 18:15:42 +0900

    update: views.py ( profile_update 함수 수정)

--- user/views.py
+++ user/views.py
@@ -14,27 +14,33 @@ def register(request):
     else:
         form = CreateUserForm()
     context = {
-        'form':form,
+        'form':form
 
 
     }
     return render(request, 'user/register.html',context)
+
 
 def profile(request):
     return render(request, 'user/profile.html')
 
 def profile_update(request):
     if request.method =='POST':
-        user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form= ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        user_form = UserUpdateForm(request.POST)
+        profile_form= ProfileUpdateForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('user-profile')
     else:
-        user_form =  UserUpdateForm(instance=request.user)
-        profile_form = ProfileUpdateForm(instance=request.user.profile) 
+        user_form =  UserUpdateForm()
+        profile_form = ProfileUpdateForm() 
 
     context ={
         'user_form' : user_form,
         'profile_form': profile_form,
         
     }
+
     return render(request, 'user/profile_update.html', context)
 
