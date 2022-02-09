14d11ac92a4a4ef8a54919e1a7e092970bc4c81b
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-09 16:05:44 +0900

    update: views.py ( profile_update 함수 생성 )

--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
 from multiprocessing import context
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from django.contrib.auth.models import Group
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -22,4 +23,18 @@ def register(request):
 def profile(request):
     return render(request, 'user/profile.html')
 
-    
+def profile_update(request):
+    if request.method =='POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form= ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    else:
+        user_form =  UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile) 
+
+    context ={
+        'user_form' : user_form,
+        'profile_form': profile_form,
+        
+    }
+    return render(request, 'user/profile_update.html', context)
+
