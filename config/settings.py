4c09f9839c7ce333380af88e67b215dcc6216723
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-09 14:30:50 +0900

    update: settings.py ( MEDIA_ROOT , MEDIA_URL 추가 )

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,11 @@ STATTICFILES_DIRS = [
 
 STATIC_ROOT = (BASE_DIR/"asert/")
 
+MEDIA_ROOT = (BASE_DIR/'media')
+
+MEDIA_URL = '/media/'
+
+
 LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
