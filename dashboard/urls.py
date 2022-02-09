09f03359420805f514afe8cd639c8817a69e03d2
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-09 20:06:51 +0900

    update: urls.py (product_update.html 연결 할 수 있게 path 경로 설정)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('staff/',views.staff, name='dashboard-staff'), # topnav.html에 url 연결시켜주기
     path('product/',views.product, name='dashboard-product'),
     path('product/delete/<int:pk>/',views.product_delete, name='dashboard-product-delete'),
+    path('product/update/<int:pk>/',views.product_update, name='dashboard-product-update'),
     path('order/',views.order, name='dashboard-order'),
 ]
  
