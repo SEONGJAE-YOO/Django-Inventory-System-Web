4291b4c0bb74e4b886680c16480b3804fa0a429b
SEONGJAE-YOO <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-02-11 16:58:52 +0900

    update: 비밀번호 찾기 기능 추가 ( 구글 확인 메일으로  비밀번호 변경 가능하게 설정)

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,11 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'yousong4243123@gmail.com'
+EMAIL_HOST_PASSWORD = 'tjdwo357!!!'
